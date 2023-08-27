798ba874e32ab249db2c4e638e760ebba65741e7
David D <i_robot73@hotmail.com>
2023-08-27 15:22:48 +0000

    08-27-2023 v01.95 - Refactored & created     'FinalDestinationValidation' function

--- complete.sh
+++ complete.sh
@@ -1,21 +1,3 @@
-#
-# 08-12-2023 v01.00 - Initial version
-# 08-12-2023 v01.50 - Added handling of 'Category'
-#   Run external program on torrent finished:
-#       '/bin/bash /scripts/complete.sh %Z %C "%N" "%D" "%F" "%L" "%R"'
-# 08-12-2023 v01.70 - Added logic to delete old log files
-# 08-12-2023 v01.75 - Added output during delete old log files
-# 08-12-2023 v01.80 - Variables in log clean-up added
-# 08-13-2023 v01.82 - Removed unnecessary redirects and cleaed-up LOG logic
-# 08-13-2023 v02.00 - Use and parse FLAGS to handle null/empty parameters
-#   (order of parameter won't matter)
-#   Run external program on torrent finished:
-#       '/bin/bash /scripts/complete.sh -n "%N" -l "%L" -f "%F" -r "%R" -c "%D" -c %C -z %Z -i "%I" -j "%J" -k "%K"'
-# 08-26-2023 v01.85 - Logging to functions & fixed purge logic
-# 08-26-2023 v01.86 - Fixed & tested logic in 'RotateLogFiles'
-# 08-26-2023 v01.90 - Logic flow corrections
-#
-
 #!/bin/bash
 app="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 #source "$app/functions.sh"
@@ -121,7 +103,7 @@ done
 #
 ###########
 
-function PrepLogFile()
+function PrepLogFile
 {
     # Ensure log file exists
     if [[ ! -e "$LOG_FILE" ]]; then
@@ -134,7 +116,7 @@ function PrepLogFile()
     fi
 }
 
-function SetupLogging()
+function SetupLogging
 {
     # The following will spit out all processes to the log
     exec 3>&1 1>>"$LOG_FILE" 2>&1
@@ -142,21 +124,22 @@ function SetupLogging()
     trap '{ set +x; } 2>/dev/null; echo -n "[$(date -Is)]  "; set -x' DEBUG
 }
 
-function RotateLogFiles()
+function PurgeLogFiles
 {
-    if [[ ! -z "$LOG_DAYS_TO_KEEP" ]]; then
+    if [[ -z "$LOG_DAYS_TO_KEEP" ]]; then
+        echo "INFO:  'LOG_DAYS_TO_KEEP' variable not set.  No clean-up performed."
+    else
         if [[ "$LOG_DAYS_TO_KEEP" -gt "0" ]]; then
             # Remove old log files
+            # NOTE:  
             find "$LOG_DIR" -maxdepth 1 -daystart -type f -iname "*$LOG_EXT" -mtime +$LOG_DAYS_TO_KEEP -exec rm -v '{}' + 
         else
             echo "WARNING:  'LOG_DAYS_TO_KEEP' must be greater than 0 to perform any work."
         fi
-    else
-        echo "INFO:  'LOG_DAYS_TO_KEEP' variable not set.  No clean-up performed."
     fi
 }
 
-function DebugToLog()
+function DebugToLog
 {
     # Echo the variables passed in via the app
     echo "Torrent: '$TOR_NAME'"
@@ -173,7 +156,7 @@ function DebugToLog()
     echo "     Tags        : $TAGS"
 }
 
-function CopyFiles()
+function CopyFiles
 {
     local DEST_PATH="$DEST"
 
@@ -182,12 +165,7 @@ function CopyFiles()
         DEST_PATH="${DEST}/${CATEGORY}"
     fi
 
-    # Make if it doesn't exist
-    if [[ ! -e "$DEST_PATH" ]]; then
-        mkdir "$DEST_PATH"
-        chown -R abc:abc "$DEST_PATH"
-        chmod -R 777 "$DEST_PATH"
-    fi
+    FinalDestinationValidation "$DEST_PATH"
 
     # Copy the folder/file(s)
     if [[ "$ROOT_PATH" != "" ]]; then
@@ -200,6 +178,25 @@ function CopyFiles()
     fi
 }
 
+fucntion FinalDestinationValidation
+{
+    # Make if it doesn't exist
+    if [[ ! -e "${1}" ]]; then
+        mkdir "${1}"
+        SetPerms "${1}"
+    # Else, verify it's actually a directory
+    elif [[ ! -d "${1}" ]]; then
+        echo "'$1' already exists but is not a directory."
+        exit 2
+    fi
+}
+
+function SetPerms
+{
+    chown -R abc:abc "${1}"
+    chmod -R 777 "${1}"
+}
+
 #########
 #
 # Logging
@@ -208,7 +205,7 @@ function CopyFiles()
 
 PrepLogFile
 SetupLogging
-RotateLogFiles
+PurgeLogFiles
 
 # (UN)Comment as desired
 DebugToLog
@@ -223,7 +220,26 @@ DebugToLog
 CopyFiles
 
 # Update permissions
-chown -R abc:abc "$FINAL_DEST"
-chmod -R 777 "$FINAL_DEST"
+SetPerms "$FINAL_DEST"
 
 echo -e 'Done.\n\n'
+exit 0
+
+#
+# 08-12-2023 v01.00 - Initial version
+# 08-12-2023 v01.50 - Added handling of 'Category'
+#   Run external program on torrent finished:
+#       '/bin/bash /scripts/complete.sh %Z %C "%N" "%D" "%F" "%L" "%R"'
+# 08-12-2023 v01.70 - Added logic to delete old log files
+# 08-12-2023 v01.75 - Added output during delete old log files
+# 08-12-2023 v01.80 - Variables in log clean-up added
+# 08-13-2023 v01.82 - Removed unnecessary redirects and cleaed-up LOG logic
+# 08-13-2023 v02.00 - Use and parse FLAGS to handle null/empty parameters
+#   (order of parameter won't matter)
+#   Run external program on torrent finished:
+#       '/bin/bash /scripts/complete.sh -n "%N" -l "%L" -f "%F" -r "%R" -c "%D" -c %C -z %Z -i "%I" -j "%J" -k "%K"'
+# 08-26-2023 v01.85 - Logging to functions & fixed purge logic
+# 08-26-2023 v01.86 - Fixed & tested logic in 'RotateLogFiles'
+# 08-26-2023 v01.90 - Logic flow corrections
+# 08-27-2023 v01.95 - Refactored & created 'FinalDestinationValidation' function
+#
